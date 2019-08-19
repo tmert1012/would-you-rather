cd17db920bf5afc06425b05067001008bbc80ff6
tmert1012 <nathan@isidorefarm.com>
2019-08-19 16:06:00 -0400

    fix: add basename to handle subdir on server

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
     render() {
         return (
-            <Router>
+            <Router basename={process.env.PUBLIC_URL}>
                 <Fragment>
                     <LoadingBar />
                     <div className='container'>
